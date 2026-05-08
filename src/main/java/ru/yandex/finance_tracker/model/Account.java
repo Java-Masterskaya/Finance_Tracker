5c71e079276d3d1a41daf72e85fa2c04623f02ac
n5n1m2 <grishkoff.volodya@yandex.ru>
2026-05-09 02:37:19 +0300

    Add users

--- src/main/java/ru/yandex/finance_tracker/model/Account.java
+++ src/main/java/ru/yandex/finance_tracker/model/Account.java
@@ -16,6 +16,9 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
     @Column(name = "name", nullable = false, length = 100)
     private String name;
     @Enumerated(EnumType.STRING)

4bb6e86853f011527e4142b4c227b5740597a1be
Vadim <zigmundfreid89@yandex.ru>
2026-05-24 05:56:25 +0300

    Добавить createTransaction в TransactionService

--- src/main/java/ru/yandex/finance_tracker/service/TransactionService.java
+++ src/main/java/ru/yandex/finance_tracker/service/TransactionService.java
@@ -1,4 +1,8 @@
 package ru.yandex.finance_tracker.service;
 
+import ru.yandex.finance_tracker.dto.input.TransactionRequest;
+import ru.yandex.finance_tracker.dto.output.TransactionInfoDto;
+
 public interface TransactionService {
+    TransactionInfoDto createTransaction(Long userId, TransactionRequest request);
 }
